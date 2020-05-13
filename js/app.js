34dab3a0d35d34d87d28e90f33c3de6778c99c11
Zoey <zoeyruss@gmail.com>
2020-05-13 09:56:34 -0400

    Redesign User Journey

--- js/app.js
+++ js/app.js
@@ -1,11 +1,49 @@
 
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
   const nav = document.querySelector('.nav-links');
   
-burger.addEventListener('click',()=>{
+menu.addEventListener('click',()=>{
   nav.classList.toggle('nav-active');
 });
 //animate links
 }
 navSlide();
+
+AOS.init({
+  duration: 1200,
+});
+document.onmousemove = function (e) {
+  document.body.style.setProperty("--x", e.clientX + "px");
+  document.body.style.setProperty("--y", e.clientY + "px");
+};
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  // options
+  cellAlign: "left",
+  contain: true,
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity(".main-carousel", {
+  // options
+});
+
+
+  var elem = document.querySelector(".main-carousel");
+			var flkty = new Flickity(elem, {
+    // options
+    cellAlign: "left",
+				contain: true,
+			});
+
+			// element argument can be a selector string
+			//   for an individual element
+			var flkty = new Flickity(".main-carousel", {
+    // options
+  });
