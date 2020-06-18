08206294e04f13a204c9d2be3ee27d6bfa899b80
Zoey <zoeyruss@gmail.com>
2020-06-18 11:08:56 -0400

    fixed issues on x-scrolll

--- js/app.js
+++ js/app.js
@@ -1,49 +1,15 @@
-
 const navSlide = () => {
-  const menu = document.querySelector('.menu');
-  const nav = document.querySelector('.nav-links');
-  
-menu.addEventListener('click',()=>{
-  nav.classList.toggle('nav-active');
-});
-//animate links
-}
+  const menu = document.querySelector(".menu");
+  const nav = document.querySelector(".nav-links");
+
+  menu.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+  //animate links
+};
 navSlide();
 
-AOS.init({
-  duration: 1200,
-});
 document.onmousemove = function (e) {
   document.body.style.setProperty("--x", e.clientX + "px");
   document.body.style.setProperty("--y", e.clientY + "px");
 };
-
-function myFunction(x) {
-  x.classList.toggle("change");
-}
-var elem = document.querySelector(".main-carousel");
-var flkty = new Flickity(elem, {
-  // options
-  cellAlign: "left",
-  contain: true,
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity(".main-carousel", {
-  // options
-});
-
-
-  var elem = document.querySelector(".main-carousel");
-			var flkty = new Flickity(elem, {
-    // options
-    cellAlign: "left",
-				contain: true,
-			});
-
-			// element argument can be a selector string
-			//   for an individual element
-			var flkty = new Flickity(".main-carousel", {
-    // options
-  });
